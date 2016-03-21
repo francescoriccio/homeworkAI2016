f9d10460707407e0a3d4af9329c16ecce4e07606
Francesco Riccio <riccio.fran@gmail.com>
2016-03-22 00:23:01 +0100

    opencv

--- board_vis.cpp
+++ board_vis.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <highgui.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp" 
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 
 #define ROI_SZ 100
 #define ROI_CANVAS ROI_SZ*0.35
